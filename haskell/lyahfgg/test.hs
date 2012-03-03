ebe39ea82938524392b21cd075c946b479d6e1c8
Mat Holroyd <matholroyd@gmail.com>
2012-03-03 15:12:57 -0800

    Fixed typo in tests

--- haskell/lyahfgg/test.hs
+++ haskell/lyahfgg/test.hs
@@ -11,9 +11,9 @@ checkSolveRPN :: String -> Float -> Test
 checkSolveRPN rpn result = TestCase (assertEqual rpn (solveRPN rpn) result)
 
 tests = TestList [
-          checkSolveRPN "3 2 + => 5" 5,
-          checkSolveRPN "3 10 - => -7"  (-7),
-          checkSolveRPN "3 1 5 + + => 9" 9,
+          checkSolveRPN "3 2 +" 5,
+          checkSolveRPN "3 10 -"  (-7),
+          checkSolveRPN "3 1 5 + +" 9,
           checkSolveRPN "10 4 3 + 2 * -" (-4)
         ]
 
