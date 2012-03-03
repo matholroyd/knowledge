9645389fb5e9f5cac9e13ba94d47b3d3d7949968
Mat Holroyd <matholroyd@gmail.com>
2012-03-03 15:09:15 -0800

    Refactored test some more

--- haskell/lyahfgg/test.hs
+++ haskell/lyahfgg/test.hs
@@ -7,11 +7,14 @@ main = do
   runTestTT tests
 
 
+checkSolveRPN :: String -> Float -> Test
+checkSolveRPN rpn result = TestCase (assertEqual rpn (solveRPN rpn) result)
+
 tests = TestList [
-          TestCase (assertEqual "3 2 + => 5"  (solveRPN "3 2 +") 5),
-          TestCase (assertEqual "3 10 - => -7"  (solveRPN "3 10 -") (-7)),
-          TestCase (assertEqual "3 1 5 + + => 9"  (solveRPN "3 1 5 + +") 9),
-          TestCase (assertEqual "10 4 3 + 2 * - => 4" (solveRPN "10 4 3 + 2 * -") (-4))
+          checkSolveRPN "3 2 + => 5" 5,
+          checkSolveRPN "3 10 - => -7"  (-7),
+          checkSolveRPN "3 1 5 + + => 9" 9,
+          checkSolveRPN "10 4 3 + 2 * -" (-4)
         ]
 
 
