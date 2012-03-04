83591ae4b8cb5dbca6dada98885cca1ca9e32e25
Mat Holroyd <matholroyd@gmail.com>
2012-03-03 16:29:29 -0800

    Spec'd first part of heathrowToLondon thing

--- haskell/lyahfgg/chapter-10.hs
+++ haskell/lyahfgg/chapter-10.hs
@@ -2,6 +2,12 @@
 
 module ChapterTen
 ( solveRPN
+, Section(..)
+, RoadSystem(..)
+, Label(..)
+, Path(..)
+, heathrowToLondon
+, optimalPath
 ) where
 
 solveRPN :: String -> Float
@@ -18,3 +24,14 @@ solveRPN = head . foldl foldingFunction [] . words
         foldingFunction xs number    = read number:xs
 
 
+data Section = Section { getA :: Int, getB :: Int, getC :: Int } deriving (Show)  
+type RoadSystem = [Section]
+
+data Label = A | B | C deriving (Show, Eq)  
+type Path = [(Label, Int)]
+
+heathrowToLondon :: RoadSystem  
+heathrowToLondon = [Section 50 10 30, Section 5 90 20, Section 40 2 25, Section 10 8 0]
+
+optimalPath :: RoadSystem -> Path
+optimalPath rs = [(A, 10)]
--- haskell/lyahfgg/test.hs
+++ haskell/lyahfgg/test.hs
@@ -1,20 +1,28 @@
 module Main where
 
-import Test.HUnit
+import Test.HUnit hiding (Path)
 import ChapterTen 
 
 main = do
-  runTestTT tests
+  runTestTT testsRPN
+  runTestTT testsOP
 
 
 checkSolveRPN :: (String, Float) -> Test
-checkSolveRPN (rpn, result) = TestCase (assertEqual rpn (solveRPN rpn) result)
+checkSolveRPN (rpn, result) = TestCase $ assertEqual rpn (solveRPN rpn) result
 
-tests = TestList $ map checkSolveRPN [
-          ("3 2 +",           5),
-          ("3 10 -",          (-7)),
-          ("3 1 5 + +",       9),
-          ("10 4 3 + 2 * -",  (-4))
+testsRPN = TestList $ map checkSolveRPN [
+          ("3 2 +",                 5),           
+          ("3 10 -",                (-7)),        
+          ("3 1 5 + +",             9),           
+          ("10 4 3 + 2 * -",        (-4)),        
+          ("2.7 ln",                0.9932518),
+          ("10 10 10 10 sum 4 /",   10.0)
         ]
 
+checkOP :: (RoadSystem, Path) -> Test
+checkOP (rs, path) = TestCase $ assertEqual "checking" (optimalPath rs) path
 
+testsOP = TestList $ map checkOP [
+          ([Section 10 20 0], [(A, 10)])
+        ]
