3568a002480eb62fc71207daec8d8295e395d403
Mat Holroyd <matholroyd@gmail.com>
2012-03-03 22:00:41 -0800

    Cleaning up solveRPN a bit

--- javascript/lyahfgg/chapter-10.js
+++ javascript/lyahfgg/chapter-10.js
@@ -1,9 +1,15 @@
-
 exports.solveRPN = function(equation) {
-  terms = equation.split(' ');
-  a = parseFloat(terms[0]);
-  b = parseFloat(terms[1]);
-  operator = terms[2];
+  var get_operator = function(op) {
+    switch(op) {
+      case '+': 
+      return function(a, b) { return a + b; }
+    }
+  }
   
-  return a + b;
+  var terms = equation.split(' '),
+      a = parseFloat(terms[0]),
+      b = parseFloat(terms[1]),
+      op = get_operator(terms[2]);
+  
+  return op(a, b);
 };
